d97090a81f4df64e1f3fca186b304fb66b9244f6
Torbjørn Årdal <torbjorn.aardal@gmail.com>
2020-12-23 12:04:53 +0100

    Call function from Lua demo

--- main.cpp
+++ main.cpp
@@ -3,32 +3,50 @@
 #include <lua/lua.hpp>
 #include <rttr/registration>
 
-void f()
+void FromLua()
 {
-    std::cout << "Hello World RTTR" << std::endl;
+    std::cout << "Hello World FromLua" << std::endl;
 }
 
 RTTR_REGISTRATION
 {
-    rttr::registration::method("f", &f);
+    rttr::registration::method("FromLua", &FromLua);
+}
+
+int LuaFunction(lua_State* L)
+{
+    std::cout << "Lua Function" << std::endl;
+    auto* method = (rttr::method*) lua_touserdata(L, lua_upvalueindex(1));
+    rttr::instance instance = {};
+    method->invoke(instance);
+    return 0;
 }
 
 int main()
 {
-    std::cout << "Hello World C++" << std::endl;
-
-    rttr::type::invoke("f", {});
-
     lua_State* L = luaL_newstate();
 
+    lua_newtable(L);
+    lua_pushvalue(L, -1);
+    lua_setglobal(L, "Global");
+
+    lua_pushvalue(L, -1);
+    for (const rttr::method& method : rttr::type::get_global_methods())
+    {
+        lua_pushstring(L, method.get_name().to_string().c_str());
+        lua_pushlightuserdata(L, (void*) &method);
+        lua_pushcclosure(L, LuaFunction, 1);
+        lua_settable(L, -3);
+    }
+
     const char* script = R"(
-        foo = "Hello World Lua"
+        Global.FromLua()
     )";
 
-    luaL_dostring(L, script);
-    lua_getglobal(L, "foo");
-    const char* foo = lua_tostring(L, -1);
-    std::cout << foo << std::endl;
+    if (luaL_dostring(L, script) != LUA_OK)
+    {
+        printf( "Error: %s\n", lua_tostring( L, -1 ) );
+    }
 
     return 0;
 }
